ea1105f4f9e721c38d4001d33018918a72a401b2
tarun-tej-2007 <taruntej947@gmail.com>
2025-03-05 11:12:34 +0530

    completed the code

--- src/components/usercard.jsx
+++ src/components/usercard.jsx
@@ -1,1 +1,22 @@
 // write the component code here
+import react from 'react';
+import './style.css'
+const Usercard = () => {
+    const profilephoto = "path/to/profilephoto.jpg" ;
+    const name = "Tarun Tej" ;
+    const mail = "adfghjklpoiuytrewq" ;
+    const phonenumber  = "1234567890" ;
+    const address =  "6889/kbgdbc/lkjhgvcdvc";
+
+    return (
+        <div>
+        <img src={profilephoto} alt="profilephoto" />
+        <h2>{name}</h2>
+        <p>Email :{mail}</p>
+        <p>Phone number :{phonenumber}</p>
+        <p>Address :{address}</p>
+        </div>
+    );
+};
+
+export default Usercard;
